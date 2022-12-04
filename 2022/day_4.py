f577e483b864dd1e7d135f1eea474233e3c7352d
Olim2508 <rahmatovolim3@gmail.com>
2022-12-04 11:48:58 +0500

    day 4 solution

--- 2022/day_4.py
+++ 2022/day_4.py
@@ -9,13 +9,27 @@ class Day4(BaseService):
 
     def _prepare_data(self, item: str):
         item = super()._prepare_data(item)
+        item = item.split(",")
+        item[0], item[1] = item[0].split("-"), item[1].split("-")
         return item
-
-    def part_1(self):
+    
+    def solution(self, all=True):
         prepared_data = super().read_input_file(self.file_path)
+        count = 0
+        for i in prepared_data:
+            left_list = [*range(int(i[0][0]), int(i[0][1]) + 1)]
+            right_list = [*range(int(i[1][0]), int(i[1][1]) + 1)]
+            if all:
+                if all(elem in left_list for elem in right_list):
+                    count += 1
+            else:
+                if any(elem in left_list for elem in right_list):
+                    count += 1
+        return count
+    
 
-    def part_2(self):
-        prepared_data = super().read_input_file(self.file_path)
-
-
-day_4 = Day4('/Users/olim/Downloads/input4.txt')
+if __name__ == '__main__':
+    day_4 = Day4('/Users/olim/Downloads/input4.txt')
+    # print("Part 1 --->", day_4.solution())
+    print("Part 2 --->", day_4.solution(all=False))
+    
